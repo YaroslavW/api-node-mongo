172f35fabdae9c3beddc1ff3ed95d65198fdad8f
YaroslavW <doctor355@yahoo.com>
2019-01-19 18:14:08 +0300

    nodemon and post request

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res) => res.send('Hello'))
+const app = express()
+app.use(bodyParser.json())
+
+const products = [
+    {
+        id: 1,
+        name: 'phone',
+        price: 300,
+    },
+    {
+        id: 2,
+        name:'tablete',
+        price: 467,
+    }
+]
+app.get('/', (req, res) => res.send('Hello world'))
+
+app.post('/products', (req, res) => {
+    products.push(req.body)
+    res.json(req.body)
+})
+app.get('/products', (req, res)=> res.json(products))
 // eslint-disable-next-line
 app.listen(3000, ()=>console.log('Listening port 3000'))
