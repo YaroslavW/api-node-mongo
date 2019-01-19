ff53292e7d114675587bc9e3512ae810b4710204
YaroslavW <doctor355@yahoo.com>
2019-01-19 18:44:30 +0300

    PUT request

--- index.js
+++ index.js
@@ -16,12 +16,20 @@ const products = [
         price: 467,
     }
 ]
-app.get('/', (req, res) => res.send('Hello world'))
+app.get('/', (req, res) => res.send('Hello'))
 
 app.post('/products', (req, res) => {
     products.push(req.body)
     res.json(req.body)
 })
 app.get('/products', (req, res)=> res.json(products))
+
+app.put('/products/:id',(req, res) => {
+    const product = products.find(p => p.id === +req.params.id)
+    const productIndex = products.indexOf(product)
+    const newProduct = { ...product, ...req.body}
+    products[productIndex] = newProduct
+    res.send({ success: true })
+})
 // eslint-disable-next-line
 app.listen(3000, ()=>console.log('Listening port 3000'))
